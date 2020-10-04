b9422845551920b299d7586e6816e9bd5ba7bf12
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-10-03 18:34:21 -0700

    playing with delete

--- src/components/ThingList/ThingList.js
+++ src/components/ThingList/ThingList.js
@@ -3,13 +3,18 @@ import Thing from "./Thing";
 import AddThing from "./AddThing";
 
 export default function ThingList(props) {
+
+  const deleteItem = (e) => {
+    console.log(e.target);
+  }
+
   return (
     <div className="container full-height" >
       <AddThing setThings={props.setThings} things={props.things} />
 
       <ul className="list-group list-group-flush">
         {props.things.map((thing) => (
-          <Thing thing={thing} key={thing.id} id={thing.id}/>
+          <Thing thing={thing} key={thing.id} id={thing.id} deleteItem={deleteItem} />
         ))}
       </ul>
     </div>
